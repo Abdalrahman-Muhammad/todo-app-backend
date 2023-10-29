f859a7456daaf1526fca2966eeb906d781ae7655
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-29 23:41:49 +0200

    Add : login route to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
+import { createUser, loginUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.route("/create").post(createUser);
+router.route("/login").post(loginUser);
 
 export default router;
