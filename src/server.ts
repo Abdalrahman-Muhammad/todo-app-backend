658b7b4e23f34edccde3c9932555e99fdb251a21
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-29 23:40:46 +0200

    Add : server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+
+import userRouter from "./routes/user.routes";
 
 const app = express();
+
+app.use(express.json());
+
+const PORT = 1337;
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("pong");
+});
+
+app.use("/user", userRouter);
+app.listen(PORT, () => {
+  console.log("Server up and running");
+});
