c387fcdad32cec190fb4db65901ab6da54f6b0e8
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-10-30 11:39:56 +0200

    Add: types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,34 @@ export interface IUser {
   email: string;
   password: string;
 }
+
+export interface IColor {
+  id: string;
+  name: string;
+  code: string;
+}
+export interface IIcon {
+  id: string;
+  name: string;
+  symbol: string;
+}
+export interface ICategory {
+  id: string;
+  name: string;
+  user: IUser | string;
+  isEditable: boolean;
+  color: IColor;
+  icon: IIcon;
+}
+
+export interface ITask {
+  id: string;
+  name: string;
+  categoryId: string;
+  userId: string;
+  isEditable: boolean;
+  isCompleted: boolean;
+  date: string;
+  createdAt: string;
+  updatedAt: string;
+}
